5a6b40aa1fdafe8463e919cc3c94396dbb19e873
atomic19 <bofh1920@gmail.com>
2020-08-08 13:25:36 -0700

    5.2 increment integer

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -2,10 +2,35 @@ from typing import List
 
 from test_framework import generic_test
 
+def plus_one_inplace(A: List[int]) -> List[int]:
+    carry = 1
+    for idx in range(len(A)-1, -1, -1):
+        sm = carry + A[idx]
+        if sm >= 10:
+            sm = sm - 10
+            carry = 1
+        else:
+            carry = 0
+        A[idx] = sm
+    if carry != 0:
+        A = [carry] + A
+    return A
 
 def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    return plus_one_inplace(A)
+    ot = []
+    carry = 1
+    for i in reversed(A):
+        sm = i + carry
+        if sm >= 10:
+            carry = 1
+            sm = sm - 10
+        else:
+            carry = 0
+        ot.append(sm)
+    if carry != 0:
+        ot.append(carry)
+    return list(reversed(ot))
 
 
 if __name__ == '__main__':
