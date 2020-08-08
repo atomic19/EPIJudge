1a02794ddbeb81338a60cd61accc10cb6faf78d7
atomic19 <bofh1920@gmail.com>
2020-08-08 14:14:04 -0700

    5.5 delete duplicate from a sorted array

--- epi_judge_python/sorted_array_remove_dups.py
+++ epi_judge_python/sorted_array_remove_dups.py
@@ -7,9 +7,26 @@ from test_framework.test_utils import enable_executor_hook
 
 # Returns the number of valid entries after deletion.
 def delete_duplicates(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
-
+    i = 0
+    first_non = None
+    while i + 1 < len(A):
+        if A[i] == A[i+1]:
+            A[i] = 'X'
+            if first_non is None:
+                first_non = i
+        i += 1
+    if first_non is not None:
+        i = first_non
+        next_num = i + 1
+        while next_num < len(A):
+            if A[next_num] != 'X':
+                A[i], A[next_num] = A[next_num], A[i]
+                i, next_num = i+1, next_num + 1
+            else:
+                next_num += 1
+        return i
+    else:
+        return len(A)
 
 @enable_executor_hook
 def delete_duplicates_wrapper(executor, A):
