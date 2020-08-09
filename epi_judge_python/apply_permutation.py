2dcd79a20488de8a2437260b76762e97d9440eb0
atomic19 <bofh1920@gmail.com>
2020-08-08 17:35:08 -0700

    5.10

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -4,7 +4,21 @@ from test_framework import generic_test
 
 
 def apply_permutation(perm: List[int], A: List[int]) -> None:
-    # TODO - you fill in here.
+    idx  = 0
+    while idx < len(perm):
+        if perm[idx] is not None:
+            run = idx
+            cur = A[run]
+            while perm[run] is not None:
+                nxt_cur = A[perm[run]]
+                nxt_run = perm[run]
+
+                A[perm[run]] = cur
+                perm[run] = None
+
+                run = nxt_run
+                cur = nxt_cur
+        idx += 1
     return
 
 
