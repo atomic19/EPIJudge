83c5280ef6772c2c9a0f421ba19ff9aec73e54b9
atomic19 <bofh1920@gmail.com>
2020-08-18 21:31:19 -0700

    updates

--- epi_judge_python/next_permutation.py
+++ epi_judge_python/next_permutation.py
@@ -4,9 +4,44 @@ from test_framework import generic_test
 
 
 def next_permutation(perm: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    r_till = None
+    for i in range(0, len(perm) - 1):
+        if perm[i] < perm[i + 1]:
+            r_till = i
 
+    print('\n', perm, 'r_till', r_till)
+
+    def find_and_sawp(idx, arr):
+        vl = arr[idx]
+        mn, found = None, None
+        for i in range(idx + 1, len(arr)):
+            if arr[i] > vl:
+                if mn is None or mn >= arr[i]:
+                    found, mn = i, arr[i]
+        if found:
+            arr[found], arr[idx] = arr[idx], arr[found]
+            arr[idx + 1:] = list(sorted(arr[idx + 1:]))
+            return True
+        return False
+
+    if r_till is None or r_till + 1 == len(perm) - 1:
+        if r_till is None:
+            return []
+        if perm[r_till] >= perm[r_till + 1]:
+            # return list(reversed(perm))
+            return []
+        else:
+            perm[r_till], perm[r_till + 1] = perm[r_till + 1], perm[r_till]
+            return perm
+    else:
+        if find_and_sawp(r_till + 1, perm):
+            return perm
+        else:
+            find_and_sawp(r_till, perm)
+            return perm
+
+
+# print([3,2,1], next_permutation([3,2,1]))
 
 if __name__ == '__main__':
     exit(
